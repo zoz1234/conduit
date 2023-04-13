05bf5bba2a78da1f997da6317321462696b41b4e
zoz1234 <89298265+zoz1234@users.noreply.github.com>
2023-04-13 17:22:20 +0200

    Update test_con.py

--- test_con.py
+++ test_con.py
@@ -11,9 +11,9 @@ class TestConduit(object):
         service = Service(executable_path=ChromeDriverManager().install())
         options = Options()
         options.add_experimental_option("detach", True)
-        # options.add_argument('--headless')
-        # options.add_argument('--no-sandbox')
-        # options.add_argument('--disable-dev-shm-usage')
+        options.add_argument('--headless')
+        options.add_argument('--no-sandbox')
+        options.add_argument('--disable-dev-shm-usage')
         self.browser = webdriver.Chrome(service=service, options=options)
         URL = "http://localhost:1667/"
         self.browser.get(URL)
