924ae1c0f59c2ee1a4b124f56b332fb321a3d3b9
zoz1234 <89298265+zoz1234@users.noreply.github.com>
2023-04-14 20:12:06 +0200

    Update func_for_test.py

--- func_for_test.py
+++ func_for_test.py
@@ -2,7 +2,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 import time
-from data import user
+from data_for_test import user
 
 def sign_in(browser):
     sign_in_btn = browser.find_element(By.LINK_TEXT, 'Sign in')
