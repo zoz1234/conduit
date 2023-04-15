6fcf04f6c83dea09806b8226226c4947edfb11f6
zoz1234 <89298265+zoz1234@users.noreply.github.com>
2023-04-15 13:06:59 +0200

    Update data_for_test.py

--- data_for_test.py
+++ data_for_test.py
@@ -3,3 +3,10 @@ user = {
     "email": "elek.teszt2023@gmail.com",
     "password": "Tesztelek23"
 }
+
+article = {
+    "title": "Teszt cikk",
+    "about": "Ismertető a cikkről",
+    "content": "A cikkünk tartalma részletesen leírva.",
+    "tags": "mytag"
+}
