83f0161c029f9bc133472ce827792b41b0b9d3d7
zoz1234 <89298265+zoz1234@users.noreply.github.com>
2023-04-22 11:30:29 +0200

    Update test_con.py

--- test_con.py
+++ test_con.py
@@ -24,57 +24,77 @@ class TestConduit(object):
         self.browser.get(URL)
         self.browser.maximize_window()
 
+
     def teardown_method(self):
         self.browser.quit()
 
+
+    # Alaklamzás betöltésének ellenőrzése
     def test_open(self):
         main_logo = self.browser.find_element(By.LINK_TEXT, 'conduit')
         assert main_logo.text == "conduit"
 
+
+    # Adatkezelési nyilatkozat használata
     def test_accept(self):
         footer = self.browser.find_element(By.TAG_NAME, 'footer')
-        accept_btn = footer.find_element(By.XPATH, '//button[@class="cookie__bar__buttons__button cookie__bar__buttons__button--accept"]')
+        accept_btn = footer.find_element(By.XPATH,
+                                         '//button[@class="cookie__bar__buttons__button cookie__bar__buttons__button--accept"]')
         accept_btn.click()
         time.sleep(1)
         cookie_panel = self.browser.find_elements(By.ID, 'cookie-policy-panel')
         assert len(cookie_panel) == 0
 
+
+    # Regisztráció
     def test_sign_up(self):
         sing_up = self.browser.find_element(By.LINK_TEXT, 'Sign up')
         sing_up.click()
 
-        username_input= WebDriverWait(self.browser, 5).until(EC.presence_of_element_located((By.XPATH, '//input[@placeholder="Username"]')))
-        email_input = WebDriverWait(self.browser, 5).until(EC.presence_of_element_located((By.XPATH, '//input[@placeholder="Email"]')))
-        password_input = WebDriverWait(self.browser, 5).until(EC.presence_of_element_located((By.XPATH, '//input[@placeholder="Password"]')))
+        username_input = WebDriverWait(self.browser, 5).until(
+            EC.presence_of_element_located((By.XPATH, '//input[@placeholder="Username"]')))
+        email_input = self.browser.find_element(By.XPATH, '//input[@placeholder="Email"]')
+        password_input = self.browser.find_element(By.XPATH, '//input[@placeholder="Password"]')
 
         username_input.send_keys(user["name"])
         email_input.send_keys(user["email"])
         password_input.send_keys(user["password"])
-        
-        sing_up_btn = WebDriverWait(self.browser, 5).until(EC.presence_of_element_located((By.XPATH, '//button[@class="btn btn-lg btn-primary pull-xs-right"]')))
+
+        sing_up_btn = self.browser.find_element(By.XPATH, '//button[@class="btn btn-lg btn-primary pull-xs-right"]')
         sing_up_btn.click()
 
-        message = WebDriverWait(self.browser, 5).until(EC.presence_of_element_located((By.XPATH, '//div[@class="swal-title"]')))
+        message = WebDriverWait(self.browser, 5).until(
+            EC.presence_of_element_located((By.XPATH, '//div[@class="swal-title"]')))
         assert message.text == "Welcome!"
 
         ok_btn = self.browser.find_element(By.XPATH, '//button[@class="swal-button swal-button--confirm"]')
         ok_btn.click()
 
-        user_name = WebDriverWait(self.browser, 5).until(EC.presence_of_all_elements_located((By.XPATH, '//a[@class="nav-link"]')))[2]
+        user_name = \
+        WebDriverWait(self.browser, 5).until(EC.presence_of_all_elements_located((By.XPATH, '//a[@class="nav-link"]')))[
+            2]
         assert user_name.text == user["name"]
-        
+
+
+    # Bejelentkezés
     def test_login(self):
         sign_in(self.browser)
 
-        user_name = WebDriverWait(self.browser, 5).until(EC.presence_of_element_located((By.XPATH, '//ul[@class="nav navbar-nav pull-xs-right"]//li[4]')))
+        user_name = WebDriverWait(self.browser, 5).until(
+            EC.presence_of_element_located((By.XPATH, '//ul[@class="nav navbar-nav pull-xs-right"]//li[4]')))
         assert user_name.text == user["name"]
-        
+
+
+    # Adatok listázása - Az első oldalon lévő cikkek címeinek listába gyűjtése
     def test_article_title_list(self):
         sign_in(self.browser)
 
-        title_list = WebDriverWait(self.browser, 5).until(EC.presence_of_all_elements_located((By.XPATH, '//div[@class="article-preview"]//a//h1')))
+        title_list = WebDriverWait(self.browser, 5).until(
+            EC.presence_of_all_elements_located((By.XPATH, '//div[@class="article-preview"]//a//h1')))
         assert len(title_list) != 0
-    
+
+
+    # Több oldalas lista bejárása - cikkek összes oldala
     def test_pages(self):
         sign_in(self.browser)
 
@@ -82,86 +102,38 @@ class TestConduit(object):
 
         for page in page_list:
             page.click()
-            active_page = WebDriverWait(self.browser, 5).until(EC.presence_of_element_located((By.XPATH, '//li[@class="page-item active"]')))
+            active_page = WebDriverWait(self.browser, 5).until(
+                EC.presence_of_element_located((By.XPATH, '//li[@class="page-item active"]')))
             assert page.text == active_page.text
-    
+
+
+    # Új adat bevitel - Új cikk felvétele
     def test_new_article(self):
         sign_in(self.browser)
         create_article(self.browser)
 
-        author = WebDriverWait(self.browser, 5).until(EC.presence_of_element_located((By.XPATH, '//a[@class="author"]')))
-        title = WebDriverWait(self.browser, 5).until(EC.presence_of_element_located((By.XPATH, '//div[@class="container"]//h1')))
+        author = WebDriverWait(self.browser, 5).until(
+            EC.presence_of_element_located((By.XPATH, '//a[@class="author"]')))
+        title = self.browser.find_element(By.XPATH, '//div[@class="container"]//h1')
 
         assert author.text == user["name"]
         assert title.text == article["title"]
-       
-#     def test_edit_article(self):
-#         sign_in(self.browser)
-#         create_article(self.browser)
 
-#         edit_btn = WebDriverWait(self.browser, 5).until(EC.presence_of_element_located((By.XPATH, '//i[@class="ion-edit"]')))
-#         edit_btn.click()
 
-#         title_input = WebDriverWait(self.browser, 5).until(EC.presence_of_element_located((By.XPATH, '//input[@placeholder="Article Title"]')))
-#         title_input.clear()
-#         title_input.send_keys("Új cím")
-#         time.sleep(2)
-
-#         publish_btn = self.browser.find_element(By.XPATH, '//button[@type="submit"]')
-#         publish_btn.click()
-
-#         title = WebDriverWait(self.browser, 15).until(EC.presence_of_element_located((By.XPATH, '//div[@class="container"]//h1')))
-
-#         assert title.text == "Új cím"   
-
-    def test_delete_article(self):
+    # Ismételt és sorozatos adatbevitel adatforrásból - Commentek hozzáadása .csv fájlból
+    def test_create_comments_from_file(self):
         sign_in(self.browser)
         create_article(self.browser)
 
-        article_url = self.browser.current_url
-        del_btn = WebDriverWait(self.browser, 5).until(EC.presence_of_element_located((By.XPATH, '//i[@class="ion-trash-a"]')))
-        del_btn.click()
-        #time.sleep(5)
-
-        assert self.browser.current_url != article_url
-        
-    def test_save_data_to_file(self):
-        sign_in(self.browser)
-        time.sleep(1)
-
-        # Az applikáció működése miatt, csak közvetlenül a felhasználó oldalára belépve
-        # gyűjti ki csak a felhasználó által publicált cikkeket. 
-        
-        self.browser.switch_to.new_window()
-        self.browser.get("http://localhost:1667/#/@testuser1")
-
-        about_list = WebDriverWait(self.browser, 15).until(
-            EC.presence_of_all_elements_located((By.XPATH, '//div[@class="article-preview"]//a//h1')))
-
-        with open('abouts.csv', 'w') as file:
-            writer = csv.writer(file)
-            for about in about_list:
-                writer.writerow([about.text])
-
-        with open('abouts.csv', 'r') as file:
-            first_row = file.readline().rstrip('\n')
-            
-            assert first_row == about_list[0].text
-            
-    def test_create_commands_from_file(self):
-        sign_in(self.browser)
-        create_article(self.browser)
-        time.sleep(1)
-        
         comments_list = []
-
         with open('comments.csv', 'r', encoding="UTF-8") as file:
             comment_table = csv.reader(file)
             for row in comment_table:
                 comments_list.append(row[0])
 
         for i in range(len(comments_list)):
-            comment_input = WebDriverWait(self.browser, 5).until(EC.presence_of_element_located((By.XPATH, '//textarea[@placeholder="Write a comment..."]')))
+            comment_input = WebDriverWait(self.browser, 5).until(
+                EC.presence_of_element_located((By.XPATH, '//textarea[@placeholder="Write a comment..."]')))
             comment_input.send_keys(comments_list[i])
 
             post_btn = self.browser.find_element(By.XPATH, '//button[@class="btn btn-sm btn-primary"]')
@@ -169,8 +141,10 @@ class TestConduit(object):
             time.sleep(1)
             new_comment = self.browser.find_elements(By.XPATH, '//p[@class="card-text"]')[0]
 
-            assert new_comment.text == comments_list[i] 
+            assert new_comment.text == comments_list[i]
 
+
+    # Meglévő adat módosítás - Felhasználó nevének módosítása
     def test_update_settings(self):
         sign_in(self.browser)
 
@@ -186,16 +160,66 @@ class TestConduit(object):
         update_btn.click()
         time.sleep(3)
 
+        # Módosítás sikerességének ellenőrzése
         title_text = self.browser.find_element(By.XPATH, '//div[@class="swal-title"]')
 
         assert title_text.text == "Update successful!"
         ok_btn = self.browser.find_element(By.XPATH, '//button[@class="swal-button swal-button--confirm"]')
         ok_btn.click()
         time.sleep(1)
-        
-        name_label = WebDriverWait(self.browser, 5).until(EC.presence_of_all_elements_located((By.XPATH, '//a[@class="nav-link"]')))[2].text
-        assert  name_label == "Teszt Géza"            
-            
+
+        # Módosított név megjelenésének ellezőrzése
+        name_label = \
+        WebDriverWait(self.browser, 5).until(EC.presence_of_all_elements_located((By.XPATH, '//a[@class="nav-link"]')))[
+            2].text
+        assert name_label == "Teszt Géza"
+
+
+    #Adat vagy adatok törlése
+    def test_delete_article(self):
+        sign_in(self.browser)
+        create_article(self.browser)
+
+        # A létrehozott új cikk oldalának URL-je
+        article_url = self.browser.current_url
+
+        # A cikk törlése
+        del_btn = WebDriverWait(self.browser, 5).until(
+            EC.presence_of_element_located((By.XPATH, '//i[@class="ion-trash-a"]')))
+        del_btn.click()
+        time.sleep(2)
+
+        # A cikk törlése után az URL-nek meg kell változnia
+        assert self.browser.current_url != article_url
+
+
+    # Adatok lementése felületről - A user1 felhasználó cikkeihez tartozó rövid leírások mentése .csv fáljba
+    def test_save_data_to_file(self):
+        sign_in(self.browser)
+        time.sleep(1)
+
+        # Az applikáció működése miatt, csak közvetlenül a felhasználó oldalára belépve
+        # gyűjti ki csak a felhasználó által publikált cikkeket.
+
+        self.browser.switch_to.new_window()
+        self.browser.get("http://localhost:1667/#/@testuser1")
+
+        about_list = WebDriverWait(self.browser, 15).until(
+            EC.presence_of_all_elements_located((By.XPATH, '//div[@class="article-preview"]//a//h1')))
+
+        with open('abouts.csv', 'w') as file:
+            writer = csv.writer(file)
+            for about in about_list:
+                writer.writerow([about.text])
+
+        # Mentés sikerességének ellenőzése az első adatsor visszaolvasásával
+        with open('abouts.csv', 'r') as file:
+            first_row = file.readline().rstrip('\n')
+
+            assert first_row == about_list[0].text
+
+
+    # Kijelentkezés
     def test_logout(self):
         sign_in(self.browser)
 
@@ -204,4 +228,3 @@ class TestConduit(object):
 
         sign_in_btn = WebDriverWait(self.browser, 5).until(EC.presence_of_element_located((By.LINK_TEXT, 'Sign in')))
         assert sign_in_btn.is_displayed()
-        
