fc9d9cbb4d3c49895b76f978bd042ba67300f03f
zoz1234 <89298265+zoz1234@users.noreply.github.com>
2023-04-22 11:33:09 +0200

    Update func_for_test.py

--- func_for_test.py
+++ func_for_test.py
@@ -4,6 +4,7 @@ from selenium.webdriver.support.wait import WebDriverWait
 import time
 from data_for_test import user, article
 
+# Bejelentkezés
 def sign_in(browser):
     sign_in_btn = browser.find_element(By.LINK_TEXT, 'Sign in')
     sign_in_btn.click()
@@ -12,22 +13,21 @@ def sign_in(browser):
         EC.presence_of_element_located((By.XPATH, '//input[@placeholder="Email"]')))
     email_input.send_keys(user["email"])
 
-    password_input = WebDriverWait(browser, 5).until(
-        EC.presence_of_element_located((By.XPATH, '//input[@placeholder="Password"]')))
+    password_input = browser.find_element(By.XPATH, '//input[@placeholder="Password"]')
     password_input.send_keys(user["password"])
 
-    sign_in_btn2 = WebDriverWait(browser, 5).until(
-        EC.presence_of_element_located((By.XPATH, '//button[@class="btn btn-lg btn-primary pull-xs-right"]')))
+    sign_in_btn2 = browser.find_element(By.XPATH, '//button[@class="btn btn-lg btn-primary pull-xs-right"]')
     sign_in_btn2.click()
 
+# Cikk létrehozása     
 def create_article(browser):
     new_art_btn = WebDriverWait(browser, 5).until(EC.presence_of_element_located((By.LINK_TEXT, 'New Article')))
     new_art_btn.click()
 
     title_input = WebDriverWait(browser, 5).until(EC.presence_of_element_located((By.XPATH, '//input[@placeholder="Article Title"]')))
-    about_input = WebDriverWait(browser, 5).until(EC.presence_of_element_located((By.XPATH, '//input[@class="form-control"]')))
-    article_text_input = WebDriverWait(browser, 5).until(EC.presence_of_element_located((By.XPATH, '//textarea[@placeholder="Write your article (in markdown)"]')))
-    tags_input = WebDriverWait(browser, 5).until(EC.presence_of_element_located((By.XPATH, '//input[@placeholder="Enter tags"]')))
+    about_input = browser.find_element(By.XPATH, '//input[@class="form-control"]')
+    article_text_input = browser.find_element(By.XPATH, '//textarea[@placeholder="Write your article (in markdown)"]')
+    tags_input = browser.find_element(By.XPATH, '//input[@placeholder="Enter tags"]')
 
     title_input.send_keys(article["title"])
     about_input.send_keys(article["about"])
@@ -36,4 +36,4 @@ def create_article(browser):
 
     publish_btn = browser.find_element(By.XPATH, '//button[@type="submit"]')
     publish_btn.click()
-
+    time.sleep(1)
